059072216b165f9d17c6ffa15ecf1f45649b68f7
L.K.H.M Lakdiw <manuthlakdiv2006@gmail.com>
2025-01-09 15:37:24 +0530

    Created student class

---
+++ Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    String name;
+    int age;
+
+    public Student(String name , int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void displayName(){
+        System.out.println("Student Name is : " + name);
+    }
+}
